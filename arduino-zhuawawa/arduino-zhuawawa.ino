623562e373954c9d17712126effa600c86fe5396
liuwei@party-time.cn <jushidai123>
2017-08-09 16:11:28 +0800

    单片机与java通信

--- arduino-zhuawawa/arduino-zhuawawa.ino
+++ arduino-zhuawawa/arduino-zhuawawa.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
 
+
+void setup() {
+   Serial.begin(9600);
+   while (!Serial) {   
+    ; // wait for serial port to connect. Needed for native USB port only
+   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  while(Serial.available()>0){
+    char cmd = Serial.read();
+    delay(2);
+    if(cmd == 'a'){
+        Serial.println("liuwei");
+    }else if(cmd == 'b'){
+        Serial.println("liuwei123");
+    }
+  }
+  
 }
