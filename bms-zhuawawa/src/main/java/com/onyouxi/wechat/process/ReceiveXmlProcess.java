925ad18310eabd0ccf5a40e094facb5bc7be0beb
liuwei@party-time.cn <jushidai123>
2017-08-20 17:21:01 +0800

    修复bug

--- bms-zhuawawa/src/main/java/com/onyouxi/wechat/process/ReceiveXmlProcess.java
+++ bms-zhuawawa/src/main/java/com/onyouxi/wechat/process/ReceiveXmlProcess.java
@@ -52,6 +52,7 @@ public class ReceiveXmlProcess {
 			//利用反射机制，调用set方法
 			//获取该实体的元类型
 			Class<?> c = Class.forName("com.onyouxi.wechat.entity.ReceiveXmlEntity");
+
 			msg = (ReceiveXmlEntity)c.newInstance();//创建这个实体的对象
 			
 			while(iter.hasNext()){
